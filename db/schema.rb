56034e9334c14020452bbc15b95f0b80bea03ca2
Dinesh Trivedi <dinesh.trivedi@vivadecora.com.br>
2015-11-02 13:55:58 -0200

    Creating Post and Comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029023952) do
+ActiveRecord::Schema.define(version: 20151102155429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20151029023952) do
     t.datetime "updated_at"
   end
 
+  create_table "comments", force: true do |t|
+    t.string   "body"
+    t.integer  "upvotes"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+
   create_table "groups", force: true do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
@@ -33,6 +43,14 @@ ActiveRecord::Schema.define(version: 20151029023952) do
   end
 
   add_index "groups", ["name"], name: "index_groups_on_name", unique: true, using: :btree
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "link"
+    t.integer  "upvotes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
