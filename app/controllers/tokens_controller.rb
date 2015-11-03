37322b73dc505b26476a8ed4682bdacb8fb6312a
Dinesh Trivedi <dinesh.trivedi@vivadecora.com.br>
2015-11-03 21:07:02 -0200

    Generating token

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,27 +1,37 @@
 class TokensController < ApplicationController
 
-  def index
-    respond_with Token.all
-  end
+    def index
+        respond_with Token.all
+    end
 
-  def create
-    respond_with Token.create(token_params.merge(user_id: params[:user_id]))
-  end
+    def create
+        respond_with Token.create(token_params.merge(user_id: params[:user_id]))
+    end
 
-  def show
-    respond_with Token.find(params[:id])
-  end
+    def show
+        respond_with Token.find(params[:id])
+    end
 
-  def update
-    token = Token.find(params[:id])
-    token.user_id = params[:user_id]
-    token.save
+    def update
+        token = Token.find(params[:id])
+        token.user_id = params[:user_id]
+        token.save
 
-    respond_with token
-  end
+        respond_with token
+    end
 
-  private
-  def token_params
-    params.require(:token).permit(:token)
-  end
+    protected
+        def generate_token
+            random_token = SecureRandom.urlsafe_base64(nil, false)
+            while Token.exists?(token: random_token) do
+                random_token = SecureRandom.urlsafe_base64(nil, false)
+            end
+
+            random_token
+        end
+
+    private
+        def token_params
+            params.require(:token).permit(:token)
+        end
 end
